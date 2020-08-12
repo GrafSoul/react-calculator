001f19e0b9e91fa1f6f5a8efb1249c2fa5cc0d36
GrafSoul <grafsoul@gmail.com>
2020-08-12 17:01:18 +0500

    feat : Renamed build folder to doc.

---
+++ doc/precache-manifest.d9b36c3069bd91f81fc8472108b03201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e09b4aa782e2d6feca9f6b4e4453458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "deea23343f1dd8db6820",
+    "url": "/static/css/main.9b5b1703.chunk.css"
+  },
+  {
+    "revision": "329a95b2b843a4f78308",
+    "url": "/static/js/2.4a5a99e5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4a5a99e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deea23343f1dd8db6820",
+    "url": "/static/js/main.d7c0d766.chunk.js"
+  },
+  {
+    "revision": "483bc82e4a06d8690839",
+    "url": "/static/js/runtime-main.7c4f77ba.js"
+  }
+]);
